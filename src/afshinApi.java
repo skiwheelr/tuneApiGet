2f5a8e326aa79b7dcb234e36744d539acad93da2
mwagner <skiwheelr@gmail.com>
2019-03-25 22:31:45 -0700

    nigga

--- src/afshinApi.java
+++ src/afshinApi.java
@@ -14,6 +14,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
+
 public class afshinApi {
     
     private final String USER_AGENT = "Mozilla/5.0";
@@ -33,12 +34,9 @@ public class afshinApi {
 	// HTTP GET request
 	private void sendGet() throws Exception {
 
-                String method = "Method=getAccountManager";
-                method = "Method=findAll";
+                String method = "Method=findAll";
+                String url="https://deltamedia.api.hasoffers.com/Apiv3/json?api_key=c5cdd08dc2cb01e8bfff8179fc6781ae2b76b65a9137a186b75905af31b9ff2c&Target=Affiliate_Offer&"+method;
                 
-		String url = "https://deltamedia.api.hasoffers.com/Apiv3/json?Target=Affiliate_Affiliate&" + method + "&api_key=c5cdd08dc2cb01e8bfff8179fc6781ae2b76b65a9137a186b75905af31b9ff2c";
-		
-                //String url="https://deltamedia.api.hasoffers.com/Apiv3/json?api_key=c5cdd08dc2cb01e8bfff8179fc6781ae2b76b65a9137a186b75905af31b9ff2c&Target=Affiliate_Offer&Method=findAll";
                 
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -60,30 +58,23 @@ public class afshinApi {
 		StringBuffer response = new StringBuffer();
 
 		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
+			response.append("\n" + inputLine);
                                              
 		}
 		in.close();
 
                 
                 JSONObject output;
-        try {
+        
             output = new JSONObject(response.toString());
-
-            JSONArray docs = output.getJSONArray("Target");
-
-            File file=new File("/fromJson.csv");
-            String csv = CDL.toString(docs);
-            FileUtils.writeStringToFile(file, csv);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } 
-                
-		//print result
-		System.out.println(response.toString());
+//
+//            JSONArray docs = output.getJSONArray("Target");
+//            File file=new File("/fromJson.csv");
+//            String csv = CDL.toString(docs);
+//            FileUtil.
+//                
+		
+	System.out.println(response.toString());
 
 	}
     
